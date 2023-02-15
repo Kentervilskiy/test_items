cc1e4b5b996d1d2b5bd05d27d3ab352b3e0a4072
Kentervilskiy <124254682+Kentervilskiy@users.noreply.github.com>
2023-02-15 16:34:05 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_cart_button(browser):
+    browser.get(link)
+    a = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(a) > 0, "button not found"
+    time.sleep(30)
